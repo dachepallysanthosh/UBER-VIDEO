4b58bb27a71f5a40b5bc5f5258bcecb59cc3ead7
dachepallysanthosh <dachepallysanthosh009@gmail.com>
2025-12-21 11:46:07 +0530

    user login done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,30 +2,54 @@ const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
-
-
+/* ================= REGISTER ================= */
 module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { fullname, email, password } = req.body;
+
+  const hashPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashPassword,
+  });
+
+  const token = user.generateAuthToken();
+
+  res.status(201).json({ token, user });
+};
+
+/* ================= LOGIN ================= */
+module.exports.loginUser = async (req, res, next) => {
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    
+    const { email, password } = req.body;
 
-    const { fullname, email, password } =  req.body;
+    const user = await userModel.findOne({ email }).select('+password');
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
 
-    const hashPassword = await userModel.hashPassword(password);
-    
-    
-    const user = await userService.createUser({
-        firstname: fullname.firstname,
-        lastname: fullname.lastname,
-        email,
-        password: hashPassword
-    });
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
 
     const token = user.generateAuthToken();
 
-    res.status(201).json({ token,user});
-
-}
+    user.password = undefined; // 🔐 hide password
+    res.status(200).json({ token, user });
+  } catch (error) {
+    next(error);
+  }
+};
